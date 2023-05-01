0a9b1948c050d5443726b90dfb0e0bcf80d82d2a
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-05-01 12:17:40 +0430

    currentWeather component has been updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useState } from 'react';
 import CurrentWeather from './components/currentWeather/CurrentWeather';
 import Search from './components/search/Search';
+import { WEATHER_API_URL } from './services/api';
 
 function App() {
+  const [currenWeather, setCurrenWeather] = useState(null);
+  const [forecastWeather, setForecastWeather] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(' ');
+
+    const currenWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${
+        import.meta.env.VITE_WEATHER_API_KEY
+      }&units=metric`
+    );
+    const forecastWeatherFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${
+        import.meta.env.VITE_WEATHER_API_KEY
+      }&units=metric`
+    );
+
+    Promise.all([currenWeatherFetch, forecastWeatherFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrenWeather({ city: searchData.label, ...weatherResponse });
+        setForecastWeather({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((error) => console.log(error));
   };
+
+  console.log(currenWeather);
+  console.log(forecastWeather);
 
   return (
     <div className='App w-full h-screen flex flex-col items-center bg-neutral-300 py-5'>
@@ -12,7 +41,8 @@ function App() {
         <Search onSearchChange={handleOnSearchChange} />
       </div>
 
-      <CurrentWeather />
+      {/* <CurrentWeather data={currenWeather} /> */}
+      {currenWeather && <CurrentWeather data={currenWeather} />}
     </div>
   );
 }
--- src/components/currentWeather/CurrentWeather.jsx
+++ src/components/currentWeather/CurrentWeather.jsx
@@ -1,18 +1,24 @@
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
   return (
-    <div className='mt-5 mb-0 w-[300px] rounded-md shadow-custom text-white bg-zinc-700 pt-0 pb-5 px-5 '>
+    <div className='mt-5 mb-0 w-[350px] rounded-md shadow-custom text-white bg-zinc-700 pt-0 pb-5 px-5 '>
       <div className='flex items-center justify-between'>
         <div>
           <p className='font-semibold text-lg leading-none m-0 tracking-[1px] '>
-            City Name
+            {data.city}
           </p>
-          <p className='font-normal text-sm leading-none m-0 '>Sunny</p>
+          <p className='font-normal text-sm leading-none m-0 '>
+            {data.weather[0].description}
+          </p>
         </div>
-        <img className='w-24' src='/icons/01d.png' alt='weather-icon' />
+        <img
+          className='w-24'
+          src={`/icons/${data.weather[0].icon}.png`}
+          alt={data.weather[0].main}
+        />
       </div>
-      <div className='flex items-center justify-between'>
+      <div className='flex items-center justify-between gap-x-4'>
         <p className='font-semibold text-7xl w-auto tracking-[-5px] my-2 mx-0'>
-          18°C
+          {Math.round(data.main.temp)}°C
         </p>
         <div className='w-full pl-5'>
           {/* Row #1 */}
@@ -22,22 +28,30 @@ const CurrentWeather = () => {
           {/* Row #2 */}
           <div className='flex justify-between '>
             <span className='text-left font-normal text-xs'>Feels like</span>
-            <span className='text-right font-semibold text-xs'>22°C </span>
+            <span className='text-right font-semibold text-xs'>
+              {Math.round(data.main.feels_like)}°C
+            </span>
           </div>
           {/* Row #3 */}
           <div className='flex justify-between '>
             <span className='text-left font-normal text-xs'>wind</span>
-            <span className='text-right font-semibold text-xs'>2 m/s</span>
+            <span className='text-right font-semibold text-xs'>
+              {Math.round(data.wind.speed)} m/s
+            </span>
           </div>
           {/* Row #4 */}
           <div className='flex justify-between '>
             <span className='text-left font-normal text-xs'>Humidity</span>
-            <span className='text-right font-semibold text-xs'>15%</span>
+            <span className='text-right font-semibold text-xs'>
+              {Math.round(data.main.humidity)} %
+            </span>
           </div>
           {/* Row #5 */}
           <div className='flex justify-between '>
             <span className='text-left font-normal text-xs'>Pressure</span>
-            <span className='text-right font-semibold text-xs'>20 hPa</span>
+            <span className='text-right font-semibold text-xs'>
+              {Math.round(data.main.pressure)} hPa
+            </span>
           </div>
         </div>
       </div>
